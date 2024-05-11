b4ed78a57b87b1d6260fc76f93848e67e2f9e032
Neylz <neylzvii@gmail.com>
2024-05-11 21:09:15 +0200

    props!

--- assets/minecraft/shaders/include/nlz/paint.glsl
+++ assets/minecraft/shaders/include/nlz/paint.glsl
@@ -1,6 +1,9 @@
 #define PI 3.14159265359
 #define SQRT_2 1.4142135624
 
+float rand(float n){return fract(sin(n) * 43758.5453123);}
+float rand(int n){return rand(float(n));}
+
 
 vec2 transform3D(vec2 coord, vec3 axis, float theta, float perspective) {
     float c = cos(theta);
@@ -27,8 +30,20 @@ vec2 transform3D(vec2 coord, vec3 axis, float theta, float perspective) {
 }
 
 vec4 colorBlend(vec4 c1, vec4 c2) {
-    return mix(c1, c2, c2.a);
+    return vec4(mix(c1, c2, c2.a).rgb, 1.0);
 }
+
+float cosInterpolate(float a, float b, float t) {
+    float ft = t * PI;
+    float f = (1.0 - cos(ft)) * 0.5;
+    return a * (1.0 - f) + b * f;
+}
+
+
+float delay(float t, float d) {
+    return (max(d, t)-d)/(1.-d);
+}
+
 
 vec4 introWave(vec2 uv, float t, vec4 color) {
     // f\left(x,\frac{\left(1-\cos\left(\pi d_{t}\right)\right)}{2}\right)
@@ -47,15 +62,15 @@ vec4 introWave(vec2 uv, float t, vec4 color) {
 }
 
 
-bool drawCircle(vec2 center, float radius, vec2 point) {
-    float dist = length(point - center);
+bool drawCircle(vec2 center, float radius, vec2 uv) {
+    float dist = length(uv - center);
     
     return dist <= radius;
 }
 
-bool drawRectangle(vec2 center, vec2 size, vec2 point, float rotation) {
+bool drawRectangle(vec2 center, vec2 size, vec2 uv, float rotation) {
     vec2 halfSize = size * 0.5;
-    vec2 translatedPoint = point - center;
+    vec2 translatedPoint = uv - center;
     
     float cosTheta = cos(rotation);
     float sinTheta = sin(rotation);
@@ -75,9 +90,9 @@ bool drawRectangle(vec2 center, vec2 size, vec2 point, float rotation) {
 }
 
 
-bool drawRoundedRectangle(vec2 center, vec2 size, vec2 point, float rotation, float cornerRadius) {
+bool drawRoundedRectangle(vec2 center, vec2 size, vec2 uv, float rotation, float cornerRadius) {
     vec2 halfSize = size * 0.5;
-    vec2 translatedPoint = point - center;
+    vec2 translatedPoint = uv - center;
     
     float cosTheta = cos(rotation);
     float sinTheta = sin(rotation);
@@ -114,19 +129,19 @@ bool drawRoundedRectangle(vec2 center, vec2 size, vec2 point, float rotation, fl
 }
 
 
-vec2 rotate(vec2 point, float angle) {
+vec2 rotate(vec2 uv, float angle) {
     return vec2(
-        point.x * cos(angle) - point.y * sin(angle),
-        point.x * sin(angle) + point.y * cos(angle)
+        uv.x * cos(angle) - uv.y * sin(angle),
+        uv.x * sin(angle) + uv.y * cos(angle)
     );
 }
 
-vec2 translate(vec2 point, vec2 translation) {
-    return point + translation;
+vec2 translate(vec2 uv, vec2 translation) {
+    return uv + translation;
 }
 
-vec2 scale(vec2 point, vec2 scale) {
-    return point * (1./scale);
+vec2 scale(vec2 uv, vec2 scale) {
+    return uv * (1./scale);
 }
 
 
@@ -194,19 +209,100 @@ bool drawVerartLogoRight(vec2 uv) {
     );
 }
 
+bool drawPropCircle(vec2 uv) {
+    return (
+        drawCircle(vec2(0., 0.), 0.1, uv)
+        && !drawCircle(vec2(0., 0.), 0.065, uv)
+    );
+}
 
-vec4 drawVerartLogo(vec2 uv) {
+bool drawPropSquare(vec2 uv) {
+    return (
+        drawRectangle(vec2(0., 0.), vec2(0.2, 0.2), uv, 0.)
+        && !drawRectangle(vec2(0., 0.), vec2(0.13, 0.13), uv, 0.)
+    );
+}
+
+bool drawPropCross(vec2 uv) {
+    return (
+        drawRectangle(vec2(0., 0.), vec2(0.2, 0.05), uv, radians(45.))
+        || drawRectangle(vec2(0., 0.), vec2(0.05, 0.2), uv, radians(45.))
+    );
+}
+
+bool drawRandomProp(vec2 uv, int seed) {
+    switch (int(rand(seed)*3.)) {
+        case 0:
+            return drawPropCircle(uv);
+        case 1:
+            return drawPropSquare(uv);
+        case 2:
+            return drawPropCross(uv);
+    }
+}
+
+bool drawPropsSplash(vec2 center, vec2 size, vec2 uv, float rotation, float dt, float seed) {
+    // draw an expanding splash of props around the center
+    
+    seed = 2.*PI*seed;
+
+    for (int i = 0 + int(seed); i < 12 + int(seed); i++) {
+        float angle = radians(30. * float(i));
+        vec2 iuv = rotate(uv + center + vec2(sin(seed)+sin(seed), sin(seed)*cos(seed))*0.1, angle);
+        iuv = translate(
+            translate(iuv, vec2(mod(rand(i), 0.2), mod(rand(i+1), 0.1)-.05)),
+            vec2(0.2* cosInterpolate(0., 1., delay(dt, 0.8)), 0.)
+        );
+        iuv = scale(iuv, size* cosInterpolate(0., 1., delay(dt, 0.5)));
+
+
+        if (drawRandomProp(iuv, i)) return true;
+    }
+}
+
+vec2 propsSplashPos[6] = vec2[6](
+    vec2(.7, 0.65),
+    vec2(1.5, -.9),
+    vec2(-0.7, 0.5),
+    vec2(-0.95, -0.85),
+    vec2(-0.7, 0.3),
+    vec2(-0.7, 0.3)
+);
+
+vec4 drawVerartProps(vec2 uv, float cma, float t) {
+    // props
+    vec4 color = vec4(vec3(1.0), 0.0);
+
+    float dt = cosInterpolate(0., 1., cma);
+
+    // if (drawPropsSplash(vec2(1.3, 0.8), vec2(0.2, 0.2), uv, 0., dt, t)) {
+    //     color.a = min(color.a + .5, 1.0);
+    // }
+
+    for (int i = 0; i < 4; i++) {
+        if (drawPropsSplash(propsSplashPos[i], vec2(0.2, 0.2), uv, 0., dt, t + float(i))) {
+            color.a = min(color.a + .5, 1.0);
+        }
+    }
+
+    return color;
+}
+
+vec4 drawVerartLogo(vec2 uv, float cma, float t) {
     // logo
     // vec4 color = vec4(vec3(vec3(uv.x,uv.y, 0.)/2. + 0.5), 1.0);
     vec4 color = vec4(0.0);
     float spacing = 0.2;
+    float delta = -(1.-cma)*0.1;
 
-    vec2 uvLeft = rotate(translate(uv, vec2(0., spacing)), radians(27.7));
+    float leftRotDelta = cosInterpolate(0., delta*250., cma);
+    vec2 uvLeft = rotate(translate(uv, vec2(0.-delta*.3, spacing-delta*0.5)), radians(27.7-leftRotDelta));
     if (drawVerartLogoLeft(uvLeft)) {
         return vec4(1.0);
     }
 
-    vec2 uvRight = rotate(translate(uv, vec2(0.08, -spacing)), radians(-22.53));
+    float rightRotDelta = delta*100.;
+    vec2 uvRight = rotate(translate(uv, vec2(0.08+delta*1.2, delta*0.7-spacing)), radians(-22.53+rightRotDelta));
     if (!drawVerartLogoLeftBg(uvLeft) && drawVerartLogoRight(uvRight)) {
         return vec4(1.0);
     }
@@ -232,14 +328,18 @@ vec4 verart(vec2 ScreenSize, vec2 coord, vec4 ColorModulator, float t) {
     fColor = introWave(uvn/2.+.5, ColorModulator.a, fColor);
     if (fColor == vec4(1.0) || fColor == vec4(0.0)) return fColor;
 
+    // props
+    fColor = colorBlend(fColor, drawVerartProps(uv, ColorModulator.a, t));
+
 
     // transform3D
     uv = -transform3D(uv, vec3(-1., 1., 0.), PI+(1.-(sin((PI/2.)*ColorModulator.a*.8+.2)))*1.3, 1.5);
 
-
     // logo
-    if (uv.x > -1. && uv.y > -1. && uv.x < 1. && uv.y < 1.) 
-        fColor = colorBlend(fColor, drawVerartLogo(uv));
+    // if (uv.x > -1. && uv.y > -1. && uv.x < 1. && uv.y < 1.) 
+    fColor = colorBlend(fColor, drawVerartLogo(uv, ColorModulator.a, t));
+    
+    
 
     return fColor;
 }
@@ -252,6 +352,6 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec2 ScreenSize = iResolution.xy;
     vec2 coord = fragCoord.xy;
     // fragColor = verart(ScreenSize, coord, vec4(1.0), iMouse.x/iResolution.x);
-    fragColor = verart(ScreenSize, coord, vec4(vec3(1.0), iMouse.x/iResolution.x), iTime/10.);
+    fragColor = verart(ScreenSize, coord, vec4(vec3(1.0), iMouse.x/iResolution.x), 0.);
 }
 #endif
